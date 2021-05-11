aafef8ae528364455d5c6488c02bff23ba97039e
alexs2011 <kir-al93@yandex.ru>
2021-05-11 14:04:55 +0700

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,11 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    s = 0
+    for line in f:
+        try:
+            number = int(line)
+            s += number
+        except ValueError:
+            pass
+    print(s)

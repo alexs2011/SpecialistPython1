85062a6c1452a69405d7ed7e5cad094c774d8c0a
alexs2011 <kir-al93@yandex.ru>
2021-05-15 15:26:45 +0700

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,26 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+while True:
+    s = input("Введите 5 целых чисел через пробел: ")
+    tokens = s.split()
+    my_list = []
+    try:
+        if len(tokens) != 5:
+            raise ValueError
+        for i in range(5):
+            my_list.append(int(tokens[i]))
+        break
+    except ValueError:
+        print("Неверные входные данные")
+
+my_list.sort()
+min = my_list[4]
+if min <= 0:
+    print("Все числа меньше 0")
+else:
+    for el in my_list:
+        if el < min and el > 0:
+            min = el
+print(min)

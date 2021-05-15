0b7ccdcf3b5f2c1df3f54605d3bd6279e2f477d5
alexs2011 <kir-al93@yandex.ru>
2021-05-15 19:05:14 +0700

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,22 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+
+
+def rand_list(n):
+    res = []
+    for _ in range(n):
+        res.append(random.randint(-20, 20))
+    return res
+
+
+lst = rand_list(10)
+print(lst)
+count = len(list(filter(lambda x: x <= 10, lst)))
+print(count)
+s = sum(filter(lambda x: x > 0, lst))
+print(s)
+n = list(filter(lambda x: x % 2 == 0 and x > 0, lst))
+avg = sum(n) / len(n)
+print(avg)
